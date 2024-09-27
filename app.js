732ac0c2ad5a189df44f7d064fe9e5bbeacce52f
Alexander <pozidevgun@gmail.com>
2024-09-27 12:08:30 +0300

    typeOf videos (array, simple< object)

--- app.js
+++ app.js
@@ -1,7 +1,51 @@
 "use strict";
-const revenue = 1000;
-const bonus = 500;
-const username = 'string';
-const bool = true;
-let res = revenue + bonus;
+// typeof function
+// function getFullName(firstname:string,secondname:string):string{
+//     return `${firstname} ${secondname}`;
+// }
+// const getFullNameArrow= (firstname:string,secondname:string):string =>{
+//     return `${firstname} ${secondname}`;
+// }
+// console.log(getFullName('sadasd','fasfa'))
+// typeof object
+// function getFullName(userInfo: {firstname:string,secondname:string}):string{
+//     return `${userInfo.firstname} ${userInfo.secondname}`;
+// }
+// const userInfo = {
+//     firstname:"Sasha",
+//     secondname:"Pozihun",
+//     age: 31,
+//     city: "Kyiv",
+// }
+// console.log(getFullName(userInfo))
+// typizetion object
+// let info :{
+//     officeId: number,
+//     isOpened: boolean,
+//     contacts:{
+//         phone: string,
+//         email: string,
+//         address:{
+//             city: string
+//         }
+//     }
+// }={
+// 	"officeId": 45,
+// 	"isOpened": false,
+// 	"contacts": {
+// 		"phone": "+38000000000",
+// 		"email": "my@gmai.com",
+// 		"address": {
+// 			"city": "Kyiv"
+// 		}
+// 	}
+// }
+// typeof array
+const arr = ['js', 'css', 'html'];
+for (const lang of arr) {
+    console.log(lang.toUpperCase());
+}
+const res = arr.filter((l) => l !== 'css')
+    .map(l => l + '! ')
+    .reduce((a, b) => a + b);
 console.log(res);
