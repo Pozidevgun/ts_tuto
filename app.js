0f1f4f18b4eddc67721b13aae282b0f226ef71c7
Alexander <pozidevgun@gmail.com>
2024-09-27 14:14:30 +0300

    tuples

--- app.js
+++ app.js
@@ -1,51 +1,10 @@
 "use strict";
-// typeof function
-// function getFullName(firstname:string,secondname:string):string{
-//     return `${firstname} ${secondname}`;
-// }
-// const getFullNameArrow= (firstname:string,secondname:string):string =>{
-//     return `${firstname} ${secondname}`;
-// }
-// console.log(getFullName('sadasd','fasfa'))
-// typeof object
-// function getFullName(userInfo: {firstname:string,secondname:string}):string{
-//     return `${userInfo.firstname} ${userInfo.secondname}`;
-// }
-// const userInfo = {
-//     firstname:"Sasha",
-//     secondname:"Pozihun",
-//     age: 31,
-//     city: "Kyiv",
-// }
-// console.log(getFullName(userInfo))
-// typizetion object
-// let info :{
-//     officeId: number,
-//     isOpened: boolean,
-//     contacts:{
-//         phone: string,
-//         email: string,
-//         address:{
-//             city: string
-//         }
-//     }
-// }={
-// 	"officeId": 45,
-// 	"isOpened": false,
-// 	"contacts": {
-// 		"phone": "+38000000000",
-// 		"email": "my@gmai.com",
-// 		"address": {
-// 			"city": "Kyiv"
-// 		}
-// 	}
-// }
-// typeof array
-const arr = ['js', 'css', 'html'];
-for (const lang of arr) {
-    console.log(lang.toUpperCase());
-}
-const res = arr.filter((l) => l !== 'css')
-    .map(l => l + '! ')
-    .reduce((a, b) => a + b);
-console.log(res);
+//tuples
+const arr = [1, 'dev'];
+// const id = arr[0];
+// const arrName = arr[1];
+// const qName = arr[2];
+arr.push('sdfsad');
+arr.pop();
+const [id, arrName] = arr;
+const arr1 = [1, 'dsafasd', false, true, false];
--- app.ts
+++ app.ts
@@ -1,57 +1,10 @@
-// typeof function
+//tuples
+const arr: [number, string]  = [1, 'dev'];
+// const id = arr[0];
+// const arrName = arr[1];
+// const qName = arr[2];
 
-// function getFullName(firstname:string,secondname:string):string{
-//     return `${firstname} ${secondname}`;
-// }
-// const getFullNameArrow= (firstname:string,secondname:string):string =>{
-//     return `${firstname} ${secondname}`;
-// }
-// console.log(getFullName('sadasd','fasfa'))
-
-
-// typeof object
-
-// function getFullName(userInfo: {firstname:string,secondname:string}):string{
-//     return `${userInfo.firstname} ${userInfo.secondname}`;
-// }
-// const userInfo = {
-//     firstname:"Sasha",
-//     secondname:"Pozihun",
-//     age: 31,
-//     city: "Kyiv",
-// }
-// console.log(getFullName(userInfo))
-
-// typizetion object
-// let info :{
-//     officeId: number,
-//     isOpened: boolean,
-//     contacts:{
-//         phone: string,
-//         email: string,
-//         address:{
-//             city: string
-//         }
-//     }
-// }={
-// 	"officeId": 45,
-// 	"isOpened": false,
-// 	"contacts": {
-// 		"phone": "+38000000000",
-// 		"email": "my@gmai.com",
-// 		"address": {
-// 			"city": "Kyiv"
-// 		}
-// 	}
-// }
- 
-// typeof array
- const arr: string[] = ['js', 'css', 'html'];
- for(const lang of arr){
-    console.log(lang.toUpperCase())
- } 
- const res = arr.filter((l:string)=>l !== 'css')
- .map(l=>l+'! ')
- .reduce((a,b) => a+b);
-
- console.log(res)
+arr.push('sdfsad')
+arr.pop()
+const [id, arrName] = arr
+const arr1: [number,string,...boolean[]] = [1, 'dsafasd', false, true, false]
