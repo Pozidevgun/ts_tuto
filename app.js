7dc604c7b0a71ada7e7db33e80a20d9a714c8b40
Alexander <pozidevgun@gmail.com>
2024-09-27 10:19:55 +0300

    lesson 10 var type

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 "use strict";
-const a = 1;
+const revenue = 1000;
+const bonus = 500;
+const username = 'string';
+const bool = true;
+let res = revenue + bonus;
+console.log(res);
--- app.ts
+++ app.ts
@@ -1,1 +1,6 @@
-const a = 1;
+const revenue: number = 1000;
+const bonus: number = 500;
+const username: string = 'string'
+const bool: boolean = true;
+let res: number = revenue+ bonus;
+console.log(res)
