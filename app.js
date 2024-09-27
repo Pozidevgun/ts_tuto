9ed42dbd1e34655acb4c2132cab81239a63585fe
Alexander <pozidevgun@gmail.com>
2024-09-27 15:03:30 +0300

    enum

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 "use strict";
-//tuples
-const arr = [1, 'dev'];
-// const id = arr[0];
-// const arrName = arr[1];
-// const qName = arr[2];
-arr.push('sdfsad');
-arr.pop();
-const [id, arrName] = arr;
-const arr1 = [1, 'dsafasd', false, true, false];
+//tuples readonly
+// const arr: readonly [number, string]  = [1, 'dev'];
+// // arr.push('sdfsad')
+// // arr.pop()
+// // const [id, arrName] = arr
+// // const arr1: [number,string,...boolean[]] = [1, 'dsafasd', false, true, false]
+// let a = 5; 
+// const arr1: ReadonlyArray<string>  = ['1', 'dev'];
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 3] = "IN_PROCESS";
+    StatusCode[StatusCode["FAILED"] = 4] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: "",
+    statusCode: StatusCode.SUCCESS,
+};
+/* 1 - успіх
+    2 -в процесі
+    3 - відключино
+*/
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+function action(status) {
+}
+action(StatusCode.SUCCESS);
+action(1);
+const res2 = 1 /* Roles.ADMIN */;
+// function test(x:{ADMIN: number}){
+// }
+// test(Roles)
--- app.ts
+++ app.ts
@@ -1,10 +1,45 @@
-//tuples
-const arr: [number, string]  = [1, 'dev'];
-// const id = arr[0];
-// const arrName = arr[1];
-// const qName = arr[2];
+//tuples readonly
 
-arr.push('sdfsad')
-arr.pop()
-const [id, arrName] = arr
-const arr1: [number,string,...boolean[]] = [1, 'dsafasd', false, true, false]
+// const arr: readonly [number, string]  = [1, 'dev'];
+
+// // arr.push('sdfsad')
+// // arr.pop()
+// // const [id, arrName] = arr
+// // const arr1: [number,string,...boolean[]] = [1, 'dsafasd', false, true, false]
+// let a = 5; 
+
+// const arr1: ReadonlyArray<string>  = ['1', 'dev'];
+enum StatusCode{
+    SUCCESS = 1,
+    IN_PROCESS = 3,
+    FAILED
+} 
+const res = {
+    message:"",
+    statusCode: StatusCode.SUCCESS,
+}
+
+/* 1 - успіх 
+    2 -в процесі 
+    3 - відключино
+*/
+if (res.statusCode === StatusCode.SUCCESS){
+
+} 
+
+function action (status: StatusCode){
+
+}
+action(StatusCode.SUCCESS)
+action(1)
+// action('f') и за того что цифровые значения используе по умолчанию 
+
+const enum Roles{
+    ADMIN =1,
+    USER = 2
+}
+const res2 = Roles.ADMIN
+// function test(x:{ADMIN: number}){
+
+// }
+// test(Roles)
