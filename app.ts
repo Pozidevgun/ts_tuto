1ce8c695dc1aa39868597f8c359391b95080f32b
Alexander <pozidevgun@gmail.com>
2024-10-09 18:41:42 +0300

    Interfaces

--- app.ts
+++ app.ts
@@ -1,31 +1,24 @@
 // lesson 3 
-// Type Aliases
-// type httpMethod = 'post' | ' get';
-
-// type coolString = string;
-// function fetchWithAuth(url: coolString, method: httpMethod): 1 | -1{
-//     return 1
-// }
-
-type User = {
+// interface
+interface User{
     name: string,
     age: number,
-    skills: string[]
+    skills: string[],
+    log:(id:number)=> string
 }
-let user: User = {
-    name: 'asd',
-    age: 34,
-    skills:['sdfasff','fsdfsdf']
+interface Role {
+    roleId: number
 }
-type Role = {
-    id: number
+interface UserWithRole extends User,Role{
+    
 }
-type FullUser  = User & Role
-
-// type fullUser  = User | Role
-let user1: FullUser = {
-    name: 'asd',
-    age: 34,
-    skills:['sdfasff','fsdfsdf'],
-    id: 1
+// interface UserWithRole extends User{
+//     roleId: number 
+// }
+let user:UserWithRole ={
+    name: 'sdfasdf',
+    age: 23,
+    skills: ['dasd','asfdd'],
+    roleId: 2,
+    log:(id)=> ''
 }
